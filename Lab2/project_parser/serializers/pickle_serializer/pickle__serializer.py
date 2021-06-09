4199fef379c5e828f55569051544ce5d92d06ce2
Mihail Bogomolov <michail.bogomolov@gmail.com>
2021-06-09 15:13:05 +0300

    repair

--- Lab2/project_parser/serializers/pickle_serializer/pickle__serializer.py
+++ Lab2/project_parser/serializers/pickle_serializer/pickle__serializer.py
@@ -1,18 +1,115 @@
-import pickle
 from project_parser.object_converter import refactor_object, restore_object
 
 
 class PickleSerializer:
     def dump(self, obj, fp): # pragma: no cover
-        with open(fp, 'wb') as outfile:
-            pickle.dump(refactor_object(obj), outfile)
+        with open(fp, 'w') as outfile:
+            outfile.write(PickleSerializer.obj_to_pickle(obj))
 
     def dumps(self, obj):
-        return pickle.dumps(refactor_object(obj))
-
-    def loads(self, s):
-        return restore_object(pickle.loads(s))
+        return PickleSerializer.obj_to_pickle(obj)
 
     def load(self, fp): # pragma: no cover
-        with open(fp, 'rb') as outfile:
-            return restore_object(pickle.load(outfile))
+        with open(fp, "r") as infile:
+            content = infile.read()
+        return PickleSerializer.pickle_to_obj(content)
+
+    def loads(self, s):
+        return PickleSerializer.pickle_to_obj(s)
+
+    @staticmethod
+    def obj_to_pickle(obj):
+        ref_obj = refactor_object(obj)
+        if type(ref_obj) is int:
+            return PickleSerializer.int_to_pickle(ref_obj)
+        elif type(ref_obj) is float:
+            return PickleSerializer.float_to_pickle(ref_obj)
+        elif type(ref_obj) is bool:
+            return PickleSerializer.bool_to_pickle(ref_obj)
+        elif type(ref_obj) is str:
+            return PickleSerializer.str_to_pickle(ref_obj)
+        elif type(ref_obj) is type(None):
+            return PickleSerializer.none_to_pickle(ref_obj)
+        elif type(ref_obj) is list:
+            return PickleSerializer.list_to_pickle(ref_obj)
+        elif type(ref_obj) is dict:
+            return PickleSerializer.dict_to_pickle(ref_obj)
+        else:
+            raise ValueError(f"{type(ref_obj)} isn't supported")
+
+    @staticmethod
+    def pickle_to_obj(pickle_str):
+        null = None
+        true = True
+        false = False
+        obj = eval(pickle_str)
+        return restore_object(obj)
+
+    @staticmethod
+    def int_to_pickle(obj):
+        return str(obj)
+
+    @staticmethod
+    def float_to_pickle(obj):
+        return str(obj)
+
+    @staticmethod
+    def bool_to_pickle(obj):
+        return str(obj).lower()
+
+    @staticmethod
+    def str_to_pickle(obj):
+        obj = obj.replace('\\', '\\\\').replace('"', '\\"')
+        return f'"{obj}"'
+
+    @staticmethod
+    def none_to_pickle(obj):
+        return 'null'
+
+    @staticmethod
+    def list_to_pickle(obj):
+        result_str = ''
+        for item in obj:
+            refactored_item = ''
+            if type(item) is int:
+                refactored_item += PickleSerializer.int_to_pickle(item)
+            elif type(item) is float:
+                refactored_item += PickleSerializer.float_to_pickle(item)
+            elif type(item) is bool:
+                refactored_item += PickleSerializer.bool_to_pickle(item)
+            elif type(item) is str:
+                refactored_item += PickleSerializer.str_to_pickle(item)
+            elif type(item) is type(None):
+                refactored_item += PickleSerializer.none_to_pickle(item)
+            elif type(item) in (list, tuple):
+                refactored_item += PickleSerializer.list_to_pickle(item)
+            elif type(item) is dict:
+                refactored_item += PickleSerializer.dict_to_json(item)
+            else:
+                raise ValueError(f"{type(item)} isn't supported")
+            result_str += refactored_item + ", "
+        return '[' + result_str[:-2] + ']'
+
+    @staticmethod
+    def dict_to_pickle(obj):
+        result_str = ''
+        for key, value in obj.items():
+            refactored_item = f'{PickleSerializer.str_to_pickle(str(key))}: '
+            if type(value) is int:
+                refactored_item += PickleSerializer.int_to_pickle(obj[key])
+            elif type(value) is float:
+                refactored_item += PickleSerializer.float_to_pickle(obj[key])
+            elif type(value) is bool:
+                refactored_item += PickleSerializer.bool_to_pickle(obj[key])
+            elif type(value) is str:
+                refactored_item += PickleSerializer.str_to_pickle(obj[key])
+            elif type(value) is type(None):
+                refactored_item += PickleSerializer.none_to_pickle(obj[key])
+            elif type(value) in (list, tuple):
+                refactored_item += PickleSerializer.list_to_pickle(obj[key])
+            elif type(value) is dict:
+                refactored_item += PickleSerializer.dict_to_pickle(obj[key])
+            else:
+                raise ValueError(f"{type(value)} isn't supported")
+            result_str += refactored_item + ", "
+        return '{' + result_str[:-2] + '}'
--- Lab2/project_parser/serializers/toml_serializer/toml_serializer.py
+++ Lab2/project_parser/serializers/toml_serializer/toml_serializer.py
@@ -1,132 +1,116 @@
-import copy
-
-import pytomlpp as toml
+from Lab2.project_parser.serializers.json_serializer.json_serializer import JsonSerializer
 from project_parser.object_converter import refactor_object, restore_object
+
 
 class TomlSerializer:
     def dump(self, obj, fp): # pragma: no cover
-        ready_obj = TomlSerializer.restructure_primitives(refactor_object(obj))
         with open(fp, 'w') as outfile:
-            toml.dump(ready_obj, outfile)
+            outfile.write(TomlSerializer.obj_to_toml(obj))
 
     def dumps(self, obj):
-        refactor_object(obj)
-        ready_obj = TomlSerializer.restructure_primitives(refactor_object(obj))
-        return toml.dumps(ready_obj)
-
-    def loads(self, s):
-        new_obj = TomlSerializer.unpack_raw_object(toml.loads(s))
-        TomlSerializer.restore_none_values(new_obj)
-        return restore_object(new_obj)
+        return TomlSerializer.obj_to_toml(obj)
 
     def load(self, fp): # pragma: no cover
-        with open(fp, 'r') as outfile:
-            return self.loads(outfile.read())
+        with open(fp, "r") as infile:
+            content = infile.read()
+        return TomlSerializer.toml_to_obj(content)
+
+    def loads(self, s):
+        return TomlSerializer.toml_to_obj(s)
 
     @staticmethod
-    def restructure_primitives(obj):
-        restructured_obj = {}
-        obj_type = TomlSerializer.get_object_type(obj)
-        if type(obj) in (int, float, bool, str, list):
-            restructured_obj['type'] = obj_type
-            restructured_obj[obj_type] = obj
-        elif type(obj) is type(None):
-            restructured_obj['type'] = obj_type
-        elif type(obj) is dict:
-            keys_types = {str(i): TomlSerializer.get_object_type(i) for i in obj}
-            restructured_obj['keys_types'] = keys_types
-            restructured_obj['type'] = obj_type
-            restructured_obj[obj_type] = {str(key): obj[key] for key in obj}
-        TomlSerializer.fix_none_values(restructured_obj)
-        return restructured_obj
+    def obj_to_toml(obj):
+        ref_obj = refactor_object(obj)
+        if type(ref_obj) is int:
+            return TomlSerializer.int_to_toml(ref_obj)
+        elif type(ref_obj) is float:
+            return TomlSerializer.float_to_toml(ref_obj)
+        elif type(ref_obj) is bool:
+            return TomlSerializer.bool_to_toml(ref_obj)
+        elif type(ref_obj) is str:
+            return TomlSerializer.str_to_toml(ref_obj)
+        elif type(ref_obj) is type(None):
+            return TomlSerializer.none_to_toml(ref_obj)
+        elif type(ref_obj) is list:
+            return TomlSerializer.list_to_toml(ref_obj)
+        elif type(ref_obj) is dict:
+            return TomlSerializer.dict_to_toml(ref_obj)
+        else:
+            raise ValueError(f"{type(ref_obj)} isn't supported")
 
     @staticmethod
-    def replace_none_value(obj):
-        obj = copy.copy(obj)
-        if type(obj) in (int, float, bool, str):
-            return obj
-        elif type(obj) is type(None):
-            return TomlSerializer.restructure_primitives(obj)
-        elif type(obj) is list:
-            for i in range(len(obj)):
-                obj[i] = TomlSerializer.replace_none_value(obj[i])
-        elif type(obj) is dict:
-            for i in obj:
-                obj[i] = TomlSerializer.replace_none_value(obj[i])
-        return obj
+    def toml_to_obj(toml_str):
+        null = None
+        true = True
+        false = False
+        obj = eval(toml_str)
+        return restore_object(obj)
 
     @staticmethod
-    def fix_none_values(obj):
-        if obj.get('type') == 'list':
-            my_list = obj['list']
-            for i in range(len(my_list)):
-                obj['list'][i] = TomlSerializer.replace_none_value(obj['list'][i])
-        elif obj.get('type') == 'dict':
-            my_dict = obj['dict']
-            for i in my_dict:
-                my_dict[i] = TomlSerializer.replace_none_value(my_dict[i])
+    def int_to_toml(obj):
+        return str(obj)
 
     @staticmethod
-    def get_object_type(obj):
-        if type(obj) is int:
-            return 'int'
-        elif type(obj) is float:
-            return 'float'
-        elif type(obj) is bool:
-            return 'bool'
-        elif type(obj) is str:
-            return 'str'
-        elif type(obj) is type(None):
-            return 'None'
-        elif type(obj) is list:
-            return 'list'
-        elif type(obj) is dict:
-            return 'dict'
+    def float_to_toml(obj):
+        return str(obj)
 
     @staticmethod
-    def unpack_raw_object(obj):
-        obj_type = obj.get('type')
-        if obj_type in ('int', 'float', 'bool', 'str', 'list'):
-            return obj[obj_type]
-        elif obj_type == 'None':
-            return None
-        elif obj_type == 'dict':
-            keys_types = obj['keys_types']
-            new_dict = {}
-            raw_dict = obj[obj_type]
-            for old_key in raw_dict:
-                new_key = TomlSerializer.convert_string(old_key, keys_types[old_key])
-                new_dict[new_key] = raw_dict[old_key]
-            return new_dict
+    def bool_to_toml(obj):
+        return str(obj).lower()
 
     @staticmethod
-    def convert_string(string, obj_type):
-        if obj_type == 'int':
-            return int(string)
-        elif obj_type == 'float':
-            return float(string)
-        elif obj_type == 'bool':
-            return bool(string)
-        elif obj_type == 'str':
-            return string
-        elif obj_type == 'None':
-            return None
-        elif obj_type == 'list':
-            return list(obj_type)
-        elif obj_type == 'dict':
-            return dict(obj_type)
+    def str_to_toml(obj):
+        obj = obj.replace('\\', '\\\\').replace('"', '\\"')
+        return f'"{obj}"'
 
     @staticmethod
-    def restore_none_values(obj):
-        if type(obj) is list:
-            for i in range(len(obj)):
-                if obj[i] == {'type': 'None'}:
-                    obj[i] = None
-                elif type(obj) in (list, dict):
-                    TomlSerializer.restore_none_values(obj[i])
-        elif type(obj) is dict:
-            for i in obj:
-                if obj[i] == {'type': 'None'}:
-                    obj[i] = None
-                elif type(obj) in (list, dict):
-                    TomlSerializer.restore_none_values(obj[i])
+    def none_to_toml(obj):
+        return 'null'
+
+    @staticmethod
+    def list_to_toml(obj):
+        result_str = ''
+        for item in obj:
+            refactored_item = ''
+            if type(item) is int:
+                refactored_item += TomlSerializer.int_to_toml(item)
+            elif type(item) is float:
+                refactored_item += TomlSerializer.float_to_toml(item)
+            elif type(item) is bool:
+                refactored_item += TomlSerializer.bool_to_toml(item)
+            elif type(item) is str:
+                refactored_item += TomlSerializer.str_to_toml(item)
+            elif type(item) is type(None):
+                refactored_item += TomlSerializer.none_to_toml(item)
+            elif type(item) in (list, tuple):
+                refactored_item += TomlSerializer.list_to_toml(item)
+            elif type(item) is dict:
+                refactored_item += TomlSerializer.dict_to_toml(item)
+            else:
+                raise ValueError(f"{type(item)} isn't supported")
+            result_str += refactored_item + ", "
+        return '[' + result_str[:-2] + ']'
+
+    @staticmethod
+    def dict_to_toml(obj):
+        result_str = ''
+        for key, value in obj.items():
+            refactored_item = f'{TomlSerializer.str_to_toml(str(key))}: '
+            if type(value) is int:
+                refactored_item += TomlSerializer.int_to_toml(obj[key])
+            elif type(value) is float:
+                refactored_item += TomlSerializer.float_to_toml(obj[key])
+            elif type(value) is bool:
+                refactored_item += TomlSerializer.bool_to_toml(obj[key])
+            elif type(value) is str:
+                refactored_item += TomlSerializer.str_to_toml(obj[key])
+            elif type(value) is type(None):
+                refactored_item += TomlSerializer.none_to_toml(obj[key])
+            elif type(value) in (list, tuple):
+                refactored_item += TomlSerializer.list_to_toml(obj[key])
+            elif type(value) is dict:
+                refactored_item += TomlSerializer.dict_to_toml(obj[key])
+            else:
+                raise ValueError(f"{type(value)} isn't supported")
+            result_str += refactored_item + ", "
+        return '{' + result_str[:-2] + '}'
