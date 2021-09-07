f9e7318c48866dd4dbfaadb6b85e4b7ca7ffed35
Mihail Bogomolov <michail.bogomolov@gmail.com>
2021-09-07 22:22:57 +0300

    Added Dockerfile and docker-compose

--- djbot/djBot/djBot/settings.py
+++ djbot/djBot/djBot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%^*+f-m1p!f@a8@%y=4p^#xw&#vf#8mhe^&57hvz_*&s*%2o-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'ShopDB',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
