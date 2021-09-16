46926f88e492c97edea31bfe8ffcfd8a8f8fafeb
Mihail Bogomolov <michail.bogomolov@gmail.com>
2021-09-16 23:56:18 +0300

    new Lab2

--- Lab2/setup.py
+++ Lab2/setup.py
@@ -1,32 +1,16 @@
-import setuptools
+#!/usr/bin/env python
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="universal-parser",
-    version="1.0",
-    author="Mihail Bogomolov",
-    author_email="michail.bogomolov@gmail.com",
-    description="Json/Pickle/Toml/Yaml parser",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/GenMiha/ISP/Lab2/project_parser",
-    project_urls={
-        "Bug Tracker": "https://github.com/GenMiha/ISP/Lab2/project_parser/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    entry_points = {
-        'console_scripts': ['format_converter=project_parser.main:main'],
-    },
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'PyYAML>=5.4.1',
-        'pytomlpp>=0.3.5',
-        ],
-    python_requires=">=3.6",
+setup(
+    name="serializer",
+    version="1.0.0",
+    description="Module allowing to serialize and deserialize objects",
+    author="Lesha Kulevich",
+    author_email='kulevich.01@mail.ru',
+    url="https://github.com/AlekseyKulevich/PythonLab02",
+    setup_requires=["wheel"],
+    install_requires=["pyyaml", "wheel", "toml"],
+    packages=["ser_create/", "."],
+    entry_points={"console_scripts": "cu=console_util:main"},
 )
